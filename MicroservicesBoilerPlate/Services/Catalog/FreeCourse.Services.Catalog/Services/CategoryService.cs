1d26e772ab54cb7e7dbad758bf45766ccb84a899
ferhatblnk <59527526+ferhatblnk@users.noreply.github.com>
2023-02-21 09:18:44 +0300

    Add files via upload

---
+++ MicroservicesBoilerPlate/Services/Catalog/FreeCourse.Services.Catalog/Services/CategoryService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Model;
+using FreeCourse.Services.Catalog.Settings;
+using FreeCourse.Shared.Dtos;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMongoCollection<Category> categoryCollection, IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnetionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+            _mapper = mapper;
+        }
+
+        public async Task<Response<List<CategoryDto>>> GetAllAsync()
+        {
+            var categoires = await _categoryCollection.Find(category => true).ToListAsync();
+            return Response<List<CategoryDto>>.Success(_mapper.Map<List<CategoryDto>>(categoires), 200);
+        }
+
+        public async Task<Response<CategoryDto>> CreateAsync(Category category)
+        {
+            await _categoryCollection.InsertOneAsync(category);
+            return Response<CategoryDto>.Success(_mapper.Map<CategoryDto>(category), 200);
+        }
+
+        public async Task<Response<CategoryDto>> GetByIdAsync(string id)
+        {
+            var category = await _categoryCollection.Find<Category>(x => x.Id == id).FirstOrDefaultAsync();
+            if (category == null)
+                return Response<CategoryDto>.Fail("Category not found", 404);
+            return Response<CategoryDto>.Success(_mapper.Map<CategoryDto>(category), 200);
+        }
+    }
+}
---
+++ MicroservicesBoilerPlate/Services/Catalog/FreeCourse.Services.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Model;
+using FreeCourse.Services.Catalog.Settings;
+using FreeCourse.Shared.Dtos;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    public interface ICategoryService
+    {
+        Task<Response<List<CategoryDto>>> GetAllAsync();
+        Task<Response<CategoryDto>> CreateAsync(Category category);
+        Task<Response<CategoryDto>> GetByIdAsync(string id);
+    }
+}
